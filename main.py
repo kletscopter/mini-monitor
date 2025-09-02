5141db52bec92098ab475011e4becbfe364525bf
kletscopter <ron.verhaege@gmail.com>
2025-09-02 17:20:49 +0200

    changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import psutil
 import platform
 import rich
 import paramiko
+import json
 
 host = "192.168.0.33"
 username = "root"
@@ -16,6 +17,19 @@ remote_file = sftp.file("/var/log/nginx/access.log", "r")
 for line in remote_file:
     rich.print(line)
 
+log_report = {"Errors": [], "Warnings": []}
+for line in remote_file:
+    if "error" in line.lower():
+        log_report["Errors"].append(line)
+    elif "warning" in line.lower():
+        log_report["Warnings"].append(line)
+
 remote_file.close()
 sftp.close()
 ssh.close()
+
+#wegschrijven naar JSON bestand
+with open("log_report.json", "w") as json_file:
+    json.dump(log_report, json_file)
+
+    rich.print(f"Log report saved to log_report.json")
